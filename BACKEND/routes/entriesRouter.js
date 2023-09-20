05cf12ddd9526201c7e290111e71ed7976d7dbf0
luguerro <137106150+luguerro@users.noreply.github.com>
2023-09-20 09:25:52 +0200

    ruta eliminar entrada

--- BACKEND/routes/entriesRouter.js
+++ BACKEND/routes/entriesRouter.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const entriesRouter = express.Router();
+const express = require('express')
+const entriesRouter = express.Router()
 
-const authUser = require('../middlewares/authUser');
+const authUser = require('../middlewares/authUser')
 const addEntry = require('../controller/entries/addEntry.js')
 
-
 // router.get('/', __________);    //!COMPLETAR PARTE DE MOSTRAR LAS ENTRADAS DE FORO
-entriesRouter.post('/', authUser, addEntry);
-// router.delete('/:entryId', authUser, ___________);  //! PARA ELIMINAR LAS ENTRADAS DE FORO
+entriesRouter.post('/', authUser, addEntry)
+entriesRouter.delete('/:entryId', authUser, deleteEntry) //! PARA ELIMINAR LAS ENTRADAS DE FORO
 
-
-
-module.exports = entriesRouter;
+module.exports = entriesRouter
