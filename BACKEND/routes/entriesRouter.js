e50f6a499280246cf47fb1db95df53f8e07cd7f8
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-09-21 09:47:00 +0200

    viasualizacion noticia 1 usuario

--- BACKEND/routes/entriesRouter.js
+++ BACKEND/routes/entriesRouter.js
@@ -3,9 +3,9 @@ const entriesRouter = express.Router();
 
 const authUser = require('../middlewares/authUser');
 const addEntry = require('../controller/entries/addEntry.js')
-const viewEntrie = require('../controller/entries/getEntrie.js')
+const viewEntryOneUser = require('../controller/entries/viewEntryOneUser.js')
 
-entriesRouter.get('/', viewEntrie);    //!COMPLETAR PARTE DE MOSTRAR LAS ENTRADAS DE FORO
+entriesRouter.get('/view/:users_user_id', viewEntryOneUser);    //!COMPLETAR PARTE DE MOSTRAR LAS ENTRADAS DE FORO
 entriesRouter.post('/', authUser, addEntry);
 // router.delete('/:entryId', authUser, ___________);  //! PARA ELIMINAR LAS ENTRADAS DE FORO
 
