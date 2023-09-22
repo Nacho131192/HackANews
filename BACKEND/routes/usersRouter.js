095161aac7eee11c2671103cfe15b9850a322107
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-09-22 12:27:59 +0200

    getUser listo

--- BACKEND/routes/usersRouter.js
+++ BACKEND/routes/usersRouter.js
@@ -6,11 +6,11 @@ const addUser = require("../controller/users/addUser.js");
 const authUser = require("../middlewares/authUser.js");
 const deleteUser = require("../controller/users/deleteUser.js");
 const updateUser = require("../controller/users/updateUser.js");
-
+const getUser = require("../controller/users/getUser.js");
 
 usersRouter.post("/", addUser);
 usersRouter.post('/login', loginUser);
-
+usersRouter.get("/dataUser/:userId", authUser, getUser)
 usersRouter.delete("/delete/:userDeleteId", authUser, deleteUser)
 usersRouter.post('/update/:userId', authUser, updateUser)
 module.exports = usersRouter
