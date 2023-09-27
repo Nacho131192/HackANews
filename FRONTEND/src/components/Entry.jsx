32aa32ff88539b9a2220ee6f952b7bb55c194bba
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-09-27 18:03:22 +0200

    FUNCIONAAAAAA

--- FRONTEND/src/components/Entry.jsx
+++ FRONTEND/src/components/Entry.jsx
@@ -3,7 +3,7 @@ export const Entry = ({ news }) => {
         <p>
             {news.new_title}
         </p>
-        {enews.new_pic ? <img src={`http://localhost:3000/uploads/${news.new_pic}`} alt="Entrada" /> : null}
+        {news.new_pic ? <img src={`http://localhost:3000/uploads/${news.new_pic}`} alt="Entrada" /> : null}
         <p> {news.new_entrance}</p>
         <p> {news.new_text}</p>
         <p> {news.new_theme}</p>
