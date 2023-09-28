56a14f5877db13a8b574f161cd10df5998d52d50
LeoERP <leona.rodriguez1985@gmail.com>
2023-09-28 12:45:25 +0200

    HABEMUS TOKEN \o/ (eso creo xD)

--- FRONTEND/src/context/LoginContext.jsx
+++ FRONTEND/src/context/LoginContext.jsx
@@ -3,15 +3,7 @@ import { createContext, useEffect, useState } from 'react';
 const LoginContext = createContext();
 
 const LoginContextProvider = ({children}) => {
-    // return <p>este es el proveedor del contexto</p>  //!  <-- este return lo pinta en el navegador
-    // return children; //!  <-- este return me muestra toda al App en el navegador
-    // const [color, setColor] = useState("blue");
-    // return (
-    // <LoginContext.Provider value={{ color, setColor }}>{children}</LoginContext.Provider> //!  <-- este return me muestra toda al App en el navegador
-    // );
-    
-
-    const [token, setToken] = useState("");
+    const [token, setToken] = useState(localStorage.getItem("token") || "");
     // const [user, setUser] = useState("");
 
     useEffect(() => {
