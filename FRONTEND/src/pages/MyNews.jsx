c5a86c102ad3d511ecf27c5ffa68d5c94d183cd1
pabloferreno <pablo@ferreno.es>
2023-10-01 11:37:23 +0200

    se me olvido hacer rama

--- FRONTEND/src/pages/MyNews.jsx
+++ FRONTEND/src/pages/MyNews.jsx
@@ -1,5 +1,11 @@
+import { useContext } from "react"
+import { LoginContext } from "../context/LoginContext"
+
 export default function MyNews() {
-  
+  const { user } = useContext(LoginContext)
+  const meId = user[0].user_id
+  console.log(meId)
+
   return (
     <h2> Mis noticias</h2>
   )
