9d8e6e3138c6c7054ebfbafbd17ffbbc913047b5
luguerro <137106150+luguerro@users.noreply.github.com>
2023-10-02 16:35:47 +0200

    falta columna lateral y header

--- FRONTEND/src/components/Footer.jsx
+++ FRONTEND/src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import './Footer.css';
+
 export default function Footer() {
     return (
         <footer>
             <h3>Footer</h3>
 
-            <b><p>Copyright © 2022 - Alligh - Hack a News</p></b>
+            <b><p>Copyright © 2023 - Hack a News</p></b>
 
 
 
