22a23dbf8e8c480d87d6c1eac8949d56399aab8b
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-10-05 23:04:14 +0200

    ..

--- FRONTEND/src/components/Header.jsx
+++ FRONTEND/src/components/Header.jsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 import logo from '../assets/logo.png';
 import Button from 'react-bootstrap/Button';
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import usuario from '../assets/usuario.png';
 
 export default function Header() {
     const { user } = useContext(LoginContext);
@@ -17,6 +20,11 @@ export default function Header() {
             {/* <h1 className='name'>Hack a News</h1> */}
 
             <ul className='nav'>
+            <DropdownButton id="dropdown-basic-button" title='{usuario}'>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
                 {/* <Link to={"/"}>
                     <li><button className='btn_header'>INICIO</button></li>
                 </Link> */}
