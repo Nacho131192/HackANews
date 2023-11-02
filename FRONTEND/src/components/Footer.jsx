cfcd4669607e8c2d5829e14549ccb0c4a5c19a22
luguerro <137106150+luguerro@users.noreply.github.com>
2023-11-02 12:34:55 +0100

    footer gradient

--- FRONTEND/src/components/Footer.jsx
+++ FRONTEND/src/components/Footer.jsx
@@ -3,11 +3,7 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer className="footer">
-            <b>
-                <p className="text-color-footer">
-                    Copyright © 2023 - Hack a News
-                </p>
-            </b>
+            <p className="text-color-footer">Copyright © 2023 - Hack a News</p>
         </footer>
     );
 }
